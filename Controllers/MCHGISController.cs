21844e2d0ce99d549bfb9628d748671cb96aa1cc
Cuong Vu Hung <cuongvuhung@gmail.com>
2023-11-23 15:50:46 +0700

    Add API IMG for all report and device

--- Controllers/MCHGISController.cs
+++ Controllers/MCHGISController.cs
@@ -22,12 +22,14 @@ namespace CBM_API.Controllers
     [Route("[controller]")]
     public class MCHGISController : ControllerBase
     {
+        static IConfiguration _config;
         public ApplicationDbContext _context;
         public MinioClient _minio;
-        public MCHGISController(ApplicationDbContext context, MinioClient minio)
+        public MCHGISController(ApplicationDbContext context, MinioClient minio,IConfiguration config)
         {
             _context = context;
             _minio = minio;
+            _config = config;
         }
         //[Authorize(Roles = "admin")]
         [HttpGet]
@@ -147,6 +149,76 @@ namespace CBM_API.Controllers
             }
         }
 
+        //[Authorize(Roles = "admin")]
+        [HttpPut("image/{id}")]
+        public async Task<IActionResult> UpdateImgItem(int id, IFormFile file)
+        {
+            try
+            {
+                if (file.ContentType.ToString().Contains("image"))
+                {
+                    MCHGIS? itemExist = await (from rec in _context.MCHGISs
+                                              where rec.Id == id
+                                              select rec).FirstOrDefaultAsync();
+                    if (itemExist == null)
+                    {
+                        return BadRequest("Device not found!");
+                    }
+                    else
+                    {
+                        string fileExtName = file.FileName.ToString().Split(".")[file.FileName.ToString().Split(".").Length - 1];
+                        string fileNameHashed = $"{Crypto.Hash(file.FileName + DateTime.Now)}.{fileExtName}";
+                        string path = $"mchgis/{id}/";
+                        string bucket = "cbm";
+                        //Xoa file cu
+                        try
+                        {
+                            string? oldfilename = (itemExist.Img + "").Split(bucket + "/")[1];// make it item.ImageURL not null and get olfile name
+                            var foundfile = await _minio.StatObjectAsync(new StatObjectArgs().WithBucket(bucket).WithObject(oldfilename));
+                            if (foundfile != null)
+                            {
+                                RemoveObjectArgs removeObjectArgs = new RemoveObjectArgs().WithBucket(bucket).WithObject(oldfilename);
+                                await _minio.RemoveObjectAsync(removeObjectArgs);
+                            }
+                        }
+                        catch { }
+                        //Tim tao moi bucket
+                        bool found = await _minio.BucketExistsAsync(new BucketExistsArgs().WithBucket(bucket));
+                        if (!found) { await _minio.MakeBucketAsync(new MakeBucketArgs().WithBucket(bucket)); }
+                        //Tao file moi
+                        try
+                        {
+                            PutObjectArgs putObjectArgs = new PutObjectArgs()
+                                .WithBucket(bucket)
+                                .WithObject(path + fileNameHashed)
+                                .WithStreamData(file.OpenReadStream())
+                                .WithObjectSize(file.Length)
+                                .WithContentType(file.ContentType);
+                            await _minio.PutObjectAsync(putObjectArgs);
+                            var publicUrl = $"{_config["Minio:Protocol"]}://{_config["Minio:Host"]}/{bucket}/{path}{fileNameHashed}";
+                            itemExist.Img = publicUrl;
+                            itemExist.UpdatedAt = DateTime.Now;
+                            itemExist.UpdatedBy = User.Claims.FirstOrDefault(ac => ac.Type == "Name")?.Value;
+                            await _context.SaveChangesAsync();
+                            return Ok(itemExist);
+                        }
+                        catch (Exception e)
+                        {
+                            return BadRequest(e.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    return BadRequest("Wrong image file");
+                }
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
         //[Authorize(Roles = "admin")]
         [HttpDelete]
         public async Task<IActionResult> DeleteItem(int id)
